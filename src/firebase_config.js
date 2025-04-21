b16a278535fc4f5758d234481531f311730930e7
Kushagra srivastava <122214707+marvillage@users.noreply.github.com>
2025-04-21 23:17:28 +0530

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,11 +1,1 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDWVs4U-MUwhw_J2BRy1TYSpLFaRnt9Kzo",
-  authDomain: "e-waste-management-2a578.firebaseapp.com",
-  projectId: "e-waste-management-2a578",
-  storageBucket: "e-waste-management-2a578.appspot.com",
-  messagingSenderId: "107981091618954629015",
-  appId: "1:107981091618954629015:web:your_app_id_here"
-};
 
-export default firebaseConfig; 
